74ca497998c7db54d098aa7fb2977aa8025c00c0
Conan Bitter <conan.bitter@gmail.com>
2025-08-16 09:30:29 +0300

    Add basic syntax

--- pandora/src/main.rs
+++ pandora/src/main.rs
@@ -1,6 +1,6 @@
 use std::fs;
 
-use crate::lexer::Lexer;
+use crate::{lexer::Lexer, parser::Parser};
 
 mod lexer;
 mod parser;
@@ -11,10 +11,13 @@ fn main() -> anyhow::Result<()> {
     //let project_source = String::from("123 453 \"Hel\\\\lo !\"  sd (tex) 23");
 
     let mut lex = Lexer::new(&project_source);
+    let mut par = Parser::new();
 
     while let Some(token) = lex.next() {
-        println!("{:?}", token);
+        //println!("===={:?}", token);
+        par.parse(token).unwrap();
     }
+    par.end_of_input().unwrap();
 
     Ok(())
 }
--- pandora/src/parser.rs
+++ pandora/src/parser.rs
@@ -7,7 +7,55 @@ pomelo! {
     %type Str String;
     %type Name String;
 
-    package ::= Int;
+    %syntax_error {
+        println!("[Error] Got {:?}, expecting:", token);
+        for extoken in expected{
+            println!("{}",extoken.name)
+        }
+        Err(())
+    }
+
+    root ::= package { println!("root ::= package"); };
+    root ::= object { println!("root ::= object"); };
+
+    package ::= KwPackage Name LBracket item_list RBracket { println!("package ::= KwPackage Name {{ item_list }}"); };
+    item_list ::= item_list item { println!("item_list ::= item_list item"); };
+    item_list ::= item { println!("item_list ::= item"); };
+
+    item ::= folder { println!("item ::= folder"); };
+    item ::= object { println!("item ::= object"); };
+
+    folder ::= Str(B) params LBracket item_list RBracket { println!("folder ::= Str({}) params {{ item_list }}",B); };
+    folder ::= Str(B) LBracket item_list RBracket { println!("folder ::= Str({}) {{ item_list }}",B); };
+    object ::= class Name(B) { println!("object ::= class Name({})",B); };
+    object ::= class Name params { println!("object ::= class Name params"); };
+    object ::= class Asterisk params { println!("object ::= class * params"); };
+    object ::= class params { println!("object ::= class params"); };
+    object ::= class Name KwImport Str { println!("object ::= class Name KwImport Str"); };
+
+    class ::= KwTex { println!("class ::= KwTex"); };
+    class ::= KwFont { println!("class ::= KwFont"); };
+    class ::= KwSprite { println!("class ::= KwSprite"); };
+    class ::= KwIntMap { println!("class ::= KwIntMap"); };
+    class ::= KwExtMap { println!("class ::= KwExtMap"); };
+
+    params ::= LParen param_list RParen { println!("params ::= ( param_list )"); };
+    param_list ::= param_list Comma param { println!("param_list ::= param_list , param"); };
+    param_list ::= param { println!("param_list ::= param"); };
+
+    param ::= Name(B) { println!("param ::= Name({})",B); };
+    param ::= Name(B) value { println!("param ::= Name({}) value",B); };
+    value ::= Str(B) { println!("value ::= Str({})",B); };
+    value ::= Name(B) { println!("value ::= Name({})",B); };
+    value ::= int_list { println!("value ::= int_list"); };
+    value ::= valobj { println!("value ::= valobj"); };
+
+    int_list ::= int_list Int { println!("int_list ::= int_list Int"); };
+    int_list ::= Int { println!("int_list ::= Int"); };
+
+    valobj ::= Name params { println!("valobj ::= Name params"); };
+
+/*    package ::= Int;
     package ::= Str;
     package ::= Name;
     package ::= LParen;
@@ -22,8 +70,9 @@ pomelo! {
     package ::= KwSprite;
     package ::= KwImport;
     package ::= KwIntMap;
-    package ::= KwExtMap;
+    package ::= KwExtMap;*/
 
 }
 
+pub use parser::Parser;
 pub use parser::Token;
